495d7bac03d3eec526aa690e7af6f33d6f4b8d3d
DerMemo <wi22b102@technikum-wien.at>
2024-11-26 19:13:28 +0100

    .

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,7 +2,7 @@ from infrastructure.stack import web_app, app_service_plan
 
 def test_web_app_uses_correct_plan():
     def check_plan(server_farm_id, plan_id):
-        assert server_farm_id == plan_id, "Web App sollte mit dem korrekten App Service Plan verknüpft sein"
+        assert server_farm_id == plan_id, "Web App should use the correct App Service Plan"
     web_app.server_farm_id.apply(lambda server_farm_id:
         app_service_plan.id.apply(lambda plan_id: check_plan(server_farm_id, plan_id))
     )
